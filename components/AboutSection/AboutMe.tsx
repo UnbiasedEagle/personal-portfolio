e76be17a09d59571af66402a106d3b570afe40c7
Saurabh Singh <imsaurabhsingh007@gmail.com>
2023-02-15 17:50:08 +0530

    update about content

--- components/AboutSection/AboutMe.tsx
+++ components/AboutSection/AboutMe.tsx
@@ -3,8 +3,8 @@ const AboutMe: React.FC = () => {
     <div>
       <h2 className='section-heading'>About Me</h2>
       <p className='max-w-4xl text-lg font-normal leading-relaxed text-gray-600'>
-        Hi, I&apos;m Saurabh Singh, a Software Developer with 1 years of
-        experience. I specialize in building robust and scalable web
+        Hi, I&apos;m Saurabh Singh, a Software Developer having around 2 years
+        of experience. I specialize in building robust and scalable web
         applications using React, Node.js, and MongoDB. I write clean and
         maintainable code and have experience working in agile teams. Let&apos;s
         work together to build cutting-edge web applications.
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Saurabh Singh | Software Engineer</title>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
+        ></meta>
       </Head>
       <Component {...pageProps} />
     </>
