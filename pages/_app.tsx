bd9662b31a7b651c7ff51c0e437ab65b18f07310
Saurabh Singh <imsaurabhsingh007@gmail.com>
2023-02-16 06:48:53 +0530

    add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,28 @@ function MyApp({ Component, pageProps }: AppProps) {
           name='viewport'
           content='width=device-width, initial-scale=1.0'
         ></meta>
+        <meta
+          name='description'
+          content='Saurabh is Software Engineer who specializes in building robust and scalable full stack web
+        applications using React, Node.js, and MongoDB.'
+        />
+        <meta name='robots' content='index,follow' />
+        <meta
+          name='keywords'
+          content='software developer, web development, full stack development, frontend development, backend development, React, Node.js'
+        />
+        <meta name='author' content='Saurabh Singh' />
+        <meta property='og:title' content='Saurabh Singh | Software Engineer' />
+        <meta
+          property='og:description'
+          content='Saurabh is Software Engineer who specializes in building robust and scalable full stack web
+          applications using React, Node.js, and MongoDB.'
+        />
+        <meta
+          property='og:image'
+          content='https://saurabhsingh.live/images/logo.jpeg'
+        />
+        <link rel='canonical' href='https://saurabhsingh.live' />
       </Head>
       <Component {...pageProps} />
     </>
