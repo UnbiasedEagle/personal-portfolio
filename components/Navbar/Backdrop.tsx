413e4c22bc217c06cec1d5a177eb3d0788dc39f8
Saurabh Singh <imsaurabhsingh007@gmail.com>
2023-02-19 16:53:43 +0530

    fix backdrop z-index issue

--- components/Navbar/Backdrop.tsx
+++ components/Navbar/Backdrop.tsx
@@ -5,8 +5,8 @@ type BackdropProps = {
 const Backdrop: React.FC<BackdropProps> = ({ show }) => {
   return (
     <div
-      className={`fixed inset-0 z-20 duration-300 ${
-        show ? 'opacity-100' : 'opacity-0'
+      className={`fixed inset-0 duration-300 ${
+        show ? 'opacity-100 z-20' : 'opacity-0'
       } bg-black/50`}
     ></div>
   );
